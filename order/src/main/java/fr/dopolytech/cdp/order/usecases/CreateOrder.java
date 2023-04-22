e5efa3f9cf4f64dbf5693e6458b241c073408288
iverly <github@iverly.net>
2023-04-22 16:54:16 +0200

    feat: add saga logic on order service
    Signed-off-by: iverly <github@iverly.net>

--- order/src/main/java/fr/dopolytech/cdp/order/usecases/CreateOrder.java
+++ order/src/main/java/fr/dopolytech/cdp/order/usecases/CreateOrder.java
@@ -2,6 +2,8 @@ package fr.dopolytech.cdp.order.usecases;
 
 import fr.dopolytech.cdp.order.domain.Order;
 import fr.dopolytech.cdp.order.domain.OrderRepository;
+import fr.dopolytech.cdp.order.infrastructure.dtos.OrderCreatedDTO;
+import fr.dopolytech.cdp.order.infrastructure.services.RabbitMQEventEmitterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +13,24 @@ public class CreateOrder {
 
     private final OrderRepository orderRepository;
 
+    private final RabbitMQEventEmitterService eventEmitterService;
+
     public Order create(Order order) {
-        return this.orderRepository.save(order);
+        Order savedOrder = this.orderRepository.save(order);
+
+        this.eventEmitterService.emit(
+            "ORDER_CREATED",
+            new OrderCreatedDTO(
+                order.getId(),
+                order.getPrice(),
+                order.getProducts()
+                    .stream()
+                    .map(product -> new OrderCreatedDTO.OrderProduct(product.getProductId(), product.getQuantity()))
+                    .toList()
+            )
+        );
+
+        return savedOrder;
     }
 
 }
