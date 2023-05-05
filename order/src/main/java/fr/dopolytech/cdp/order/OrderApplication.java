8e6997e369ad554b3cd2ebe096dfc7299e135aaf
iverly <github@iverly.net>
2023-05-05 16:00:57 +0200

    feat(order): transform yml to properties & add env variables
    Signed-off-by: iverly <github@iverly.net>

--- order/src/main/java/fr/dopolytech/cdp/order/OrderApplication.java
+++ order/src/main/java/fr/dopolytech/cdp/order/OrderApplication.java
@@ -2,12 +2,10 @@ package fr.dopolytech.cdp.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@EnableDiscoveryClient
 public class OrderApplication {
 
     public static void main(String[] args) {
