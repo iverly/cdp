2c4f927651601cab1c32bb048e0fd054e9031028
iverly <github@iverly.net>
2023-05-05 16:01:27 +0200

    feat(catalog): add env variables
    Signed-off-by: iverly <github@iverly.net>

--- catalog/src/main/java/fr/dopolytech/cdp/catalog/CatalogApplication.java
+++ catalog/src/main/java/fr/dopolytech/cdp/catalog/CatalogApplication.java
@@ -2,12 +2,10 @@ package fr.dopolytech.cdp.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
-@EnableDiscoveryClient
 public class CatalogApplication {
 
     public static void main(String[] args) {
